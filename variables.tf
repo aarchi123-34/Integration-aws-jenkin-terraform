399be7bff6e77cd943075776e66b82e731c6fce0
aarchi123-34 <Aarchi.Miglani@ibm.com>
2026-01-29 11:52:13 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+  default     = "ami-0532be01f26a3de55"
+}
